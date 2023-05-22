ad1bbe99c17c5750e726a8866f2d1ec4f7fb4ba5
Tim Waters <chippy2005@gmail.com>
2023-05-22 12:57:00 +0100

    add fake user data for front page tables

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
+    @latest_surveys = Survey.last(5).reverse
   end
 
 end
--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,2 +1,10 @@
 class Survey < ApplicationRecord
+
+  def observation_count
+    attributes.values.select(&:present?).count - 6
+  end
+
+  def fake_username
+    ["Alexander", "Benjamin", "Christopher", "David", "Ethan", "Gabriel", "Henry", "James", "John", "Michael", "Abigail", "Emily", "Elizabeth", "Hannah", "Isabella", "Jasmine", "Julia", "Lily", "Olivia", "Sophia"].sample
+  end
 end
