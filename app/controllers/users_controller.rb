46cb12af10fac1618c483c4985b9f432594abc1b
Tim Waters <chippy2005@gmail.com>
2023-06-05 11:57:44 +0100

    order users table by number of contributions so it acts like a score card

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
 
   #TODO Pagination, sorting etc
   def index
-    @users = User.page(params[:page])
+    @users = User.order("contribution_count desc").page(params[:page])
   end
 
   def show
