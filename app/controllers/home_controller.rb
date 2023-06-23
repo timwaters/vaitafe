7c20f9a149b41f55bd94d6776ad8abc71092790b
Tim Waters <chippy2005@gmail.com>
2023-06-23 17:34:54 +0100

    fix front page active users table

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
     @latest_surveys = Survey.order("updated_at asc").last(5).reverse
-    @active_users = User.order("contribution_count asc").limit(5).reverse
+    @active_users = User.order("contribution_count desc").limit(5)
   end
   
   def about
